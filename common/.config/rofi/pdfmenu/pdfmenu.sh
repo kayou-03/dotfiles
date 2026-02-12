8a00055c65d3b07cbaa48cbaca5a89a5c5f08c1b
kayou-03 <noah.serre03@gmail.com>
2026-02-12 10:12:28 +0100

    font

--- common/.config/rofi/pdfmenu/pdfmenu.sh
+++ common/.config/rofi/pdfmenu/pdfmenu.sh
@@ -2,4 +2,4 @@
 
 # Find PDFs in the Documents folder and pipe them to rofi.
 # The selected PDF is then opened with zathura.
-find ~/Documents -type f -name "*.pdf" | rofi -dmenu -i -p "Ouvrir PDF:" -theme ~/.config/rofi/appmenu/appmenu.rasi | xargs -I {} zathura "{}"
+find /home/noah/ -type f -name "*.pdf" | rofi -dmenu -i -p "Ouvrir PDF:" -theme ~/.config/rofi/appmenu/appmenu.rasi | xargs -I {} zathura "{}"
